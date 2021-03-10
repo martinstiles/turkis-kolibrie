1b4a4421d2944e5a77863a8779bfe997d77bc5f5
bowlerman <oskar@goldhahn.info>
2021-03-10 16:27:49 +0100

    followers per party init

---
+++ statistics/followers_per_party_advanced.py
@@ -0,0 +1,64 @@
+import os
+# import matplotlib.pyplot as plt
+from pathlib import Path
+
+PATH = Path(os.path.dirname(__file__))
+PATH_TO_USERS = PATH.parent / "data_retrieval" / "data" / "parties"
+PATH_TO_TWEETS = PATH.parent / "data_retrieval" / "data" / "tweets"
+
+SCREEN_NAMES = [
+    "Arbeiderpartiet",
+    "Hoyre",
+    "SVparti",
+    "Venstre",
+    "Partiet",
+    "Raudt",
+    "frp_no",
+    "KrFNorge",
+    "Senterpartiet"
+]
+
+followers = {}
+for party in SCREEN_NAMES:
+    with open(PATH_TO_USERS / (party + ".txt")) as users_file:
+        users_list = users_file.readlines()
+        users_list = [user.strip() for user in users_list]
+        users_list.sort()
+        followers[party] = users_list
+
+counts = {}
+progress = 0
+for party1 in followers:
+    for user in followers[party1]:
+        passed_party = False
+        failed = False
+        parties_string = ""
+        for party2 in followers:
+            progress += 1
+            if not progress % 1000:
+                print(progress)
+            if party2 == party1:
+                passed_party = True
+            if user in followers[party2]:
+                if passed_party:
+                    parties_string += party2
+                else:
+                    failed = True
+                    break
+        if not failed:
+            if parties_string in counts:
+                counts[parties_string] += 1
+            else:
+                counts[parties_string] = 1
+
+print(counts)
+total = 0
+delete_queue = []
+for parties_string in counts:
+    total += counts[parties_string]
+
+counts = {parties_string: counts[parties_string] for parties_string in counts if counts[parties_string] >= 20}
+
+print(counts)
+
+print(total)
