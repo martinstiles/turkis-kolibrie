f02ddc3aad2f9c95566a000d32a42998c2228c20
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2021-03-10 13:32:17 +0100

    make script to generate barplot for number of users per party

--- statistics/follower_per_party.py
+++ statistics/follower_per_party.py
@@ -1,27 +1,53 @@
 import os
+import json
 import matplotlib.pyplot as plt
 from pathlib import Path
 
 PATH = Path(os.path.dirname(__file__))
 PATH_TO_USERS = PATH.parent / "data_retrieval" / "data" / "parties"
 PATH_TO_TWEETS = PATH.parent / "data_retrieval" / "data" / "tweets"
+PARTY_FILES = [
+    "Arbeiderpartiet",
+    "frp_no",
+    "Hoyre",
+    "KrFNorge",
+    "Partiet",
+    "Raudt",
+    "Senterpartiet",
+    "SVparti",
+    "Venstre",
+]
+PARTY_FILE_TO_DISPLAY_NAME = {
+    "Arbeiderpartiet": "AP",
+    "frp_no": "FRP",
+    "Hoyre": "Høyre",
+    "KrFNorge": "KRF",
+    "Partiet": "MDG",
+    "Raudt": "Rødt",
+    "Senterpartiet": "SP",
+    "SVparti": "SV",
+    "Venstre": "Venstre",
+}
 
-print(PATH_TO_TWEETS)
+x_parties = []
+y_num_users = []
+
+for party_file in PARTY_FILES:
+    with open(PATH_TO_USERS / (party_file + ".txt"), "r") as file:
+        users = file.readlines()
+        num_users = len(users)
+
+        x_parties.append(PARTY_FILE_TO_DISPLAY_NAME[party_file])  # Remove .json
+        y_num_users.append(num_users)
 
 
 
 plt.style.use('ggplot')
-
-x = ["AP", "SV"]
-energy = [59000, 20000]
-
-x_pos = [i for i, _ in enumerate(x)]
-
-plt.bar(x, energy, color='green')
+plt.bar(x_parties, y_num_users, color='green')
 plt.xlabel("Party")
-plt.ylabel("#Users")
-plt.title("Number of user per party")
+plt.ylabel("Users")
+plt.title("Number of users per party")
 
-plt.xticks(x_pos, x)
+#plt.xticks(x_pos, x)
 
 plt.show()
