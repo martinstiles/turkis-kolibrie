8e769454ac9e816a346165ebaafcdf6251b67168
bowlerman <oskar@goldhahn.info>
2021-04-08 17:20:49 +0200

    lagt til metodikk

--- statistics/follower_per_party_visualizations.py
+++ statistics/follower_per_party_visualizations.py
@@ -1,6 +1,7 @@
 import os
 import json
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 PATH = Path(os.path.dirname(__file__))
@@ -32,9 +33,11 @@ def parties_string_to_list(parties_string):
 distributions = [(parties_string_to_list(comb), distributions[comb]) for comb in distributions]
 
 
-def find_occurrences(included=[], excluded=[]):
+def find_occurrences(included=[], excluded=[], min_follows=0, max_follows=9):
     tot = 0
     for distribution in distributions:
+        if not(min_follows <= len(distribution[0]) <= max_follows):
+            continue
         for party in included:
             if party not in distribution[0]:
                 break
@@ -49,17 +52,29 @@ def find_occurrences(included=[], excluded=[]):
 
 L = len(SCREEN_NAMES)
 total_users = find_occurrences()
-print("Total users:  {}".format(total_users))
-print()
-print("Percentage of users following...")
-for party in SCREEN_NAMES:
-    print("{:{width1}}:{:{width2}.1%}".format(party, find_occurrences([party])/total_users, width1=17, width2=6))
-print()
-print("Percentage of users following...")
 for party1 in SCREEN_NAMES:
-    print("{} also following...".format(party1))
     followers_party1 = find_occurrences([party1])
+    data = {}
+    partier = []
     for party2 in SCREEN_NAMES:
         if party1 != party2:
-            print("{:{width1}}:{:{width2}.1%}".format(party2, find_occurrences([party1, party2])/followers_party1, width1=17, width2=6))
-    print()
+            partier.append(party2)
+            data[party2] = find_occurrences([party1, party2])/followers_party1
+    plt.style.use('ggplot')
+    plt.bar(data.keys(), [data[key] for key in partier], color='green')
+    plt.xlabel("Party")
+    plt.ylabel("Users")
+    plt.title(party1)
+    #plt.show()
+
+data = {}
+for party in SCREEN_NAMES:
+    data[party]  = find_occurrences([party], max_follows=1)
+plt.style.use('ggplot')
+plt.bar(data.keys(), [data[key] for key in SCREEN_NAMES], color='green')
+plt.xlabel("Party")
+plt.ylabel("Users")
+plt.title("Brukere som kun følger ett parti")
+#plt.show()
+
+print(sum([data[key] for key in SCREEN_NAMES]))
